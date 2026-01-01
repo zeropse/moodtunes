985b7516b1e96c7645b8d4715ba2bda153b1f3d5
zeropse <129523087+zeropse@users.noreply.github.com>
2026-01-01 23:50:17 +0530

    add mode toggle

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
+import { ModeToggle } from "@/theme/mode-toggle";
+
 export default function HomePage() {
   return (
     <main>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of the application.</p>
+      <ModeToggle />
     </main>
   );
 }
---
+++ src/theme/mode-toggle.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { IconMoon, IconSun } from "@tabler/icons-react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const isDark = theme === "dark";
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={toggleTheme}
+      className="cursor-pointer relative text-muted-foreground"
+    >
+      <IconSun
+        className={cn(
+          "h-[1.2rem] w-[1.2rem] transition-all",
+          isDark ? "scale-0 -rotate-90" : "scale-100 rotate-0"
+        )}
+      />
+      <IconMoon
+        className={cn(
+          "absolute h-[1.2rem] w-[1.2rem] transition-all",
+          isDark ? "scale-100 rotate-0" : "scale-0 rotate-90"
+        )}
+      />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
